0e6e8d2f2aaa7da35e9961823b3f21778eb089e7
Melquisedeque <melquisedequevital@gmail.com>
2026-09-13 20:54:55 -0300

    Inicializa projeto Spring Boot

---
+++ src/main/java/br/edu/ifpb/pweb2/dindin/DindinApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.dindin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DindinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DindinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifpb/pweb2/dindin/DindinApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifpb.pweb2.dindin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DindinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
